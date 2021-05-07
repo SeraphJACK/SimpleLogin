e4561f11f5b5ffc0f4a33671870b1d9bb0f0e283
SeraphJACK <seraphjack@outlook.com>
2021-05-07 21:51:55 +0800

    Fix

--- src/main/java/top/seraphjack/simplelogin/client/ClientLoader.java
+++ src/main/java/top/seraphjack/simplelogin/client/ClientLoader.java
@@ -1,5 +1,6 @@
 package top.seraphjack.simplelogin.client;
 
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -8,7 +9,7 @@ import top.seraphjack.simplelogin.SimpleLogin;
 import top.seraphjack.simplelogin.network.MessageLogin;
 import top.seraphjack.simplelogin.network.NetworkLoader;
 
-@Mod.EventBusSubscriber(modid = SLConstants.MODID)
+@Mod.EventBusSubscriber(modid = SLConstants.MODID, value = Dist.CLIENT)
 public final class ClientLoader {
 
     @SubscribeEvent
