8cb97444b711db337783a9ea09639873254dc3b1
SeraphJACK <seraphjack@outlook.com>
2021-05-08 12:39:10 +0800

    Cleanup

--- src/main/java/top/seraphjack/simplelogin/server/handler/plugins/RestrictGameType.java
+++ src/main/java/top/seraphjack/simplelogin/server/handler/plugins/RestrictGameType.java
@@ -3,11 +3,8 @@ package top.seraphjack.simplelogin.server.handler.plugins;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.world.GameType;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
-import top.seraphjack.simplelogin.server.capability.CapabilityLoader;
 import top.seraphjack.simplelogin.server.handler.HandlerPlugin;
 import top.seraphjack.simplelogin.server.handler.Login;
-import top.seraphjack.simplelogin.server.handler.PlayerLoginHandler;
-import top.seraphjack.simplelogin.server.storage.Position;
 import top.seraphjack.simplelogin.server.storage.SLStorage;
 
 public final class RestrictGameType implements HandlerPlugin {
