baf7cf597e762f5da6ecc0726991d89d7eb4cd0e
httpsoxfonchpemnetdir <128884574+httpsoxfonchpemnetdir@users.noreply.github.com>
2023-03-26 23:09:43 +0000

    Pending changes exported from your codespace

--- src/main/java/top/seraphjack/simplelogin/client/ClientLoader.java
+++ src/main/java/top/seraphjack/simplelogin/client/ClientLoader.java
@@ -1,27 +1,3 @@
-package top.seraphjack.simplelogin.client;
+package top.serephjack.simplelogin.client;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
-import net.minecraftforge.client.event.RegisterClientCommandsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import top.seraphjack.simplelogin.SLConstants;
-import top.seraphjack.simplelogin.SimpleLogin;
-import top.seraphjack.simplelogin.network.MessageLogin;
-import top.seraphjack.simplelogin.network.NetworkLoader;
 
-@Mod.EventBusSubscriber(modid = SLConstants.MODID, value = Dist.CLIENT)
-public final class ClientLoader {
-
-    @SubscribeEvent
-    public static void joinServer(ClientPlayerNetworkEvent.LoggingIn event) {
-        if (event.getConnection().isMemoryConnection()) return;
-        SimpleLogin.logger.debug("Sending login packet to the server...");
-        NetworkLoader.INSTANCE.sendToServer(new MessageLogin(PasswordHolder.instance().password()));
-    }
-
-    @SubscribeEvent
-    public static void onClientRegisterCommand(RegisterClientCommandsEvent event) {
-        ChangePasswordCommand.register(event.getDispatcher());
-    }
-}
